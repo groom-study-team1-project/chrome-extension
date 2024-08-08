d9d279f204610f380253a9ad6f78e3e7a5fe21b1
Seio924 <seio9241@gmail.com>
2024-08-09 01:05:11 +0900

    feat(#9): model controller 처리

--- scripts/lecture/views/LectureListView.js
+++ scripts/lecture/views/LectureListView.js
@@ -1,25 +1,23 @@
 const LECTURE_LIST_VIEW = "[TAG] Lecture List - ";
 
 class LectureListView extends View {
+  constructor() {
+    super(qs("#app"));
+    console.log(LECTURE_LIST_VIEW, "Construct Test");
 
-    constructor() {
-        super(qs("#app"));
-        console.log(LECTURE_LIST_VIEW, "Construct Test");
+    this.bindEvents();
+  }
 
-        this.bindEvents();
+  bindEvents() {
+    on(this.element, "click", (event) => this.handleClick(event));
+  }
+
+  handleClick(event) {
+    const target = event.target.closest("._2dEI31");
+
+    if (target && qs("svg._30zp6b", target)) {
+      const collapseDiv = target.nextElementSibling;
+      this.emit("validateDuplication", { liTag: target, collapseDiv });
     }
-
-    bindEvents() {
-        on(this.element, "click", event => this.handleClick(event));
-    }
-
-    handleClick(event) {
-        const target = event.target.closest('._2dEI31');
-
-        if (target && qs('svg._30zp6b', target)) {
-            const collapseDiv = target.nextElementSibling;
-            this.emit('validateDuplication', { liTag: target, collapseDiv });
-        }
-    }
-
+  }
 }
