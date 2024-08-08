35a383835deffbf7b25cdba9866019b27227135f
jihwan <jihwan1535@nate.com>
2024-08-09 00:36:00 +0900

    refactor(#10): code 최적화

--- scripts/lecture/views/LectureListView.js
+++ scripts/lecture/views/LectureListView.js
@@ -14,19 +14,12 @@ class LectureListView extends View {
     }
 
     handleClick(event) {
-        const target = event.target;
+        const target = event.target.closest('._2dEI31');
 
-        if (target.classList.contains('_2dEI31') && qs('svg._30zp6b', target)) {
+        if (target && qs('svg._30zp6b', target)) {
             const collapseDiv = target.nextElementSibling;
             this.emit('validateDuplication', { liTag: target, collapseDiv });
         }
-
-        if (target.classList.contains('cz0wtv') && qs('svg._30zp6b', target.parentElement)) {
-            const liTag = target.parentElement;
-            const collapseDiv = liTag.nextElementSibling;
-            this.emit('validateDuplication', { liTag, collapseDiv });
-        }
-
     }
 
 }
