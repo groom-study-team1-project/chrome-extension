82571a230147cb65e1de09c8da058fa22710068f
jihwan <jihwan1535@nate.com>
2024-08-09 00:08:28 +0900

    feat(#10): 강의 정보 추출 후 이벤트 발행

--- scripts/lecture/views/LectureListView.js
+++ scripts/lecture/views/LectureListView.js
@@ -15,16 +15,18 @@ class LectureListView extends View {
 
     handleClick(event) {
         const target = event.target;
-        const isLiTag = target.tagName === 'LI'
-            && target.classList.contains('_2dEI31')
-            && qs('svg', target);
-        const isDivTag = target.tagName === 'DIV'
-            && target.classList.contains('cz0wtv')
-            && qs('svg._30zp6b', target.parentElement);
 
-        if (isLiTag || isDivTag) {
-            console.log(target, "test");
+        if (target.classList.contains('_2dEI31') && qs('svg._30zp6b', target)) {
+            const collapseDiv = target.nextElementSibling;
+            this.emit('validateDuplication', { liTag: target, collapseDiv });
         }
+
+        if (target.classList.contains('cz0wtv') && qs('svg._30zp6b', target.parentElement)) {
+            const liTag = target.parentElement;
+            const collapseDiv = liTag.nextElementSibling;
+            this.emit('validateDuplication', { liTag, collapseDiv });
+        }
+
     }
 
 }
