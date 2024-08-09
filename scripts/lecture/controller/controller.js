dd99cff7f98ef3efd3d19486a505077756e81213
jihwan <jihwan1535@nate.com>
2024-08-10 04:11:15 +0900

    feat: Lecture Main Duplicate

--- scripts/lecture/controller/controller.js
+++ scripts/lecture/controller/controller.js
@@ -1,50 +1,56 @@
-const CONTROLLER_TAG = "[TAG] Controller - ";
-
 class Controller {
-  
-  constructor(lecture, { lectureListView: lectureListView }) {
-    this.lecture = lecture;
-    this.lectureListView = lectureListView;
-    console.log(CONTROLLER_TAG, "construct test");
 
-    this.subscribeEvents();
-  }
+    constructor(lecture, { lectureListView }) {
+        this.lecture = lecture;
+        this.lectureListView = lectureListView;
 
-  subscribeEvents() {
-    this.lectureListView.on("validateDuplication", (event) =>
-      this.validateDuplication(event.detail)
-    );
-  }
+        this.subscribeEvents();
+    }
 
-  validateDuplication({ liTag, collapseDiv }) {
-    const lectureId = qs("div.unqGd-", liTag).innerText;
-    const titleTags = qsAll("span._29VsDL", collapseDiv);
-    titleTags.forEach((title) => {
-      const lectureTitle = title.innerText.trim();
-      if (this.lecture.isDuplicate({ lectureId, lectureTitle })) {
-        this.process(title);
-      }
-    });
-  }
+    subscribeEvents() {
+        this.lectureListView.on("validateDuplication", (event) =>
+            this.validateDuplication(event.detail)
+        );
+    }
 
-  process(title) {
-    chrome.storage.sync.get(['toggleState'], (result) => {
-      const parentLiTagOfTitle = title.closest("._3ZcQ2Q");
-      parentLiTagOfTitle.style.display = result.toggleState ? "none" : "block";
+    validateDuplication({liTag, collapseDiv}) {
+        const lectureId = qs("div.unqGd-", liTag).innerText;
+        const titleTags = qsAll("span._29VsDL", collapseDiv);
 
-      if (!title.classList.contains("duplication")) {
-        title.classList.add("duplication");
-        this.draw(title);
-      }
-    });
-  }
+        titleTags.forEach((title) => {
+            const lectureTitle = title.innerText.trim();
 
-    draw(title) {
-      const duplicateText = document.createElement("span");
-      duplicateText.textContent = " (중복)";
-      duplicateText.style.color = "red";
+            if (this.lecture.isDuplicate({lectureId, lectureTitle})) {
+                this.process(title);
+            }
+        });
+    }
 
-      title.appendChild(duplicateText);
+    process(title) {
+        chrome.storage.sync.get(['toggleState'], (result) => {
+            this.toggleProcess({ title, state: result.toggleState });
+
+            if (!title.classList.contains("duplication")) {
+                title.classList.add("duplication");
+                this.drawProcess(title);
+            }
+        });
+    }
+
+    toggleProcess({ title, state }) {
+        const currentUrl = window.location.href;
+        const parentInfo = currentUrl.includes("lesson") ? "._2J4-8M.V6OMcC" : "._3ZcQ2Q";
+        const parentLiTagOfTitle = title.closest(parentInfo);
+
+        parentLiTagOfTitle.style.display = state ? "none" : "block";
+    }
+
+    drawProcess(title) {
+        const duplicateText = document.createElement("span");
+        duplicateText.textContent = " (중복)";
+        duplicateText.style.color = "red";
+
+        title.appendChild(duplicateText);
     }
 
 }
